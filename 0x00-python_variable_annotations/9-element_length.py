41b7a5ce0a340e0a241c7ea0d3201b4c9657d9f2
chika nwaigwe goodluck <chikagnwaigwe@gmail.com>
2023-10-05 08:34:12 -0700

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Let's duck type an iterable object
+"""
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """I it walks likea a duck and it quacks like a duck
